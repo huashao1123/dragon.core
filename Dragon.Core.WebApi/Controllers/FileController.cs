ece5ac0daaef4edffa39727ee96b54981f4d8b40
lhh <longwang289@foxmail.com>
2022-11-08 17:47:43 +0800

    增加文件上传控制器

---
+++ Dragon.Core.WebApi/Controllers/FileController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dragon.Core.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpGet("sysfile/getlist")]
+        public string GetFileList()
+        {
+            return "test";
+        }
+        [HttpPost("sysfile/add")]
+        public async Task<bool> AddFileAsunc()
+        {
+            return true;
+        }
+    }
+}
